d964d80dd254866b06db5f8fac4453612bc6264a
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-07-08 23:12:21 -0300

    Chore: Add YieldFarmingWithNFT

---
+++ contracts/RewardsDistributionRecipient.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.9;
+
+// Inheritance
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// https://docs.synthetix.io/contracts/source/contracts/rewardsdistributionrecipient
+contract RewardsDistributionRecipient is Ownable {
+    address public rewardsDistribution;
+
+    modifier onlyRewardsDistribution() {
+        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
+        _;
+    }
+
+    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
+        rewardsDistribution = _rewardsDistribution;
+    }
+}
