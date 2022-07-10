2d5a45bbaca98b12a255d16cd04c50f35b38b781
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-07-10 14:34:55 -0300

    Update: Renamed files

--- contracts/tokens/StakeToken.sol
+++ contracts/tokens/StakeToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenERC1155 is ERC1155, Ownable {
+contract StakeToken is ERC1155, Ownable {
   uint256 public constant GOLD = 0;
   uint256 public constant SILVER = 1;
 
